99279926cd0f91d2f7621c9e8639f39324860aa5
unknown <sergei25102007@bk.ru>
2025-04-08 18:40:15 +0400

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(['item'])
+defineEmits(['addToCart'])
+</script>
+
+
+
+<template>
+    <div class="card">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+        <p>Цена: <b>{{ item.price }}</b> </p>
+        <button @click="$emit('addToCart')" style="color: blue;">Купить</button>
+        
+    </div>
+
+</template>
+
+<style scoped>
+.card {
+    border: 1px solid black;
+    width: 300px;
+    aspect-ratio: 1;
+    margin: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { reactive } from 'vue'
+import Child from "../components/Child.vue"
+let goods = reactive([
+    {
+        name: "Колбаса",
+        description: "Вареная с кусками мяса",
+        price: 2000
+    },
+    {
+        name: "Хлеб",
+        description: "Высший сорт",
+        price: 100
+    },
+
+]
+)
+let cart = reactive(
+    []
+)
+function addToCart(item) {
+    cart.push(item)
+
+}
+</script>
+
+<template>
+    <h2>Витрина товаров</h2>
+<div class="wrapper">
+    <Child v-for="item in goods" :item = 'item' @add-to-cart="">
+    
+    </Child>
+</div>
+
+</template>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+</style>
