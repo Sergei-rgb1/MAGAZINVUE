4b48dc619f379595238ea4636119689cdf157d3f
unknown <sergei25102007@bk.ru>
2025-04-22 18:46:02 +0400

    korzina

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,19 +1,27 @@
 <script setup>
-defineProps(['item'])
-defineEmits(['addToCart'])
+import { ref } from 'vue';
+
+const props = defineProps(['item'])
+let emit = defineEmits(['addToCart'])
+
+let amount = ref(0)
+
+function addItemToCart() {
+   let toCart = {...props.item}
+   toCart.amount = amount.value 
+   emit('addToCart', toCart)
+}
 </script>
-
-
 
 <template>
     <div class="card">
         <h3>{{ item.name }}</h3>
         <p>{{ item.description }}</p>
         <p>Цена: <b>{{ item.price }}</b> </p>
-        <button @click="$emit('addToCart')" style="color: blue;">Купить</button>
         
+        <input class="number2" type="number" v-model="amount">
+        <button @click="addItemToCart" style="color: blue;">Купить</button>
     </div>
-
 </template>
 
 <style scoped>
@@ -22,5 +30,12 @@ defineEmits(['addToCart'])
     width: 300px;
     aspect-ratio: 1;
     margin: 20px;
+}
+
+.number2 {
+    color: rgb(255, 0, 0);
+    background-color: black;
+    height: 30px;
+    width: 50px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,33 +5,49 @@ let goods = reactive([
     {
         name: "Колбаса",
         description: "Вареная с кусками мяса",
-        price: 2000
+        price: 2000,
+        amount:0
     },
     {
         name: "Хлеб",
         description: "Высший сорт",
-        price: 100
+        price: 100,
+        amount:0
     },
-
 ]
 )
 let cart = reactive(
     []
 )
 function addToCart(item) {
-    cart.push(item)
+    if (!cart.length) {
+    cart.push (item)
+}else {
+    for (let i = 0; i < cart.length; i++) {
+        console.log(cart[i].name)
+        console.log(item.name);
+        
+       if (cart[i].name==item.name) {
+        cart[item].amount=cart[item].amount+item.amount
+       } else {
+        cart.push.item
+       }
+       
+    }
+}
 
 }
+
+
 </script>
 
 <template>
     <h2>Витрина товаров</h2>
 <div class="wrapper">
-    <Child v-for="item in goods" :item = 'item' @add-to-cart="">
-    
+    <Child v-for="item in goods" :item="item" @addToCart="addToCart">
     </Child>
 </div>
-
+{{ cart }}
 </template>
 
 <style scoped>
@@ -39,5 +55,4 @@ function addToCart(item) {
     display: flex;
     flex-wrap: wrap;
 }
-
 </style>
